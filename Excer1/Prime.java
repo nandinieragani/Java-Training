752ac5748ccbdb9c947e6158de5d82e0afa15a31
nandinieragani <nandinieragani02@gmail.com>
2024-06-14 13:27:02 +0530

    Excersice
    Excercise

---
+++ Excer1/Prime.java
@@ -0,0 +1,30 @@
+package com.hcltech.Excer1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prime {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// for user input take bufferedreader
+		BufferedReader r1 = new BufferedReader(new InputStreamReader(System.in));
+		// Enter the number
+		System.out.println("Enter the number:");
+		int n = Integer.parseInt(r1.readLine());
+		// checking the condition if n<= 2 and n!=0 print prime
+
+		if (n <= 2 && n != 0) {
+			System.out.println("prime");
+		}
+
+		// Checking prime condition
+		else if (n % 2 != 0 && n % 3 != 0) {
+			System.out.println(n + " is a prime");
+		} else {
+			System.out.println(n + " not a prime");
+		}
+
+	}
+
+}
---
+++ Excer1/Second_Banglore.java
@@ -0,0 +1,30 @@
+package com.hcltech.Excer1;
+import java.util.Scanner;
+public class Second_Banglore {
+	public static void main(String[] args) {
+		//Enter the two strings
+		System.out.println("Enter the two strings:");
+		//using the scanner to take inputs from user
+		Scanner sc = new Scanner(System.in);
+		String str1 = sc.next();
+		String str2 = sc.next();
+		char ch = '"';
+		//string 3 concatinate the two strings with technologies
+		String str3 = ch + str1.concat(" ").concat("Technologies").concat(" ").concat(str2) + ch;
+         //compare the contents in str1 and str2 if it equals banglore print ABC Technologies Banglore
+		if (str1.equals("ABC") && str2.equals("Banglore")) {
+			System.out.println(str3);
+
+		}
+        // if it equals mumbai print ABC Technologies Mumbai
+		else if (str1.equals("ABC") && str2.equals("Mumbai")) {
+			System.out.println(str3);
+		} 
+		//Else print invalid or not matches the input
+		else {
+			System.out.println("No input matches");
+		}
+	}
+
+}
+
